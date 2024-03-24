997eb7ea42d1bbb88cd91c1d8a123e083b246cb1
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2024-03-24 19:56:23 +0000

    change

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import pandas as pd
 import chromadb
 from sentence_transformers import SentenceTransformer
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 # Load the dataset
 @st.cache_data
